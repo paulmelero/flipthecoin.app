9ddd02015c2032a8a42e8af737e609abdfc8adcb
Paul <paul.melero@backmarket.com>
2024-09-14 02:15:59 +0200

    add new animation

--- coin/index.ts
+++ coin/index.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 import * as CANNON from 'cannon-es';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
@@ -9,10 +10,13 @@ const cannonToThreeQuaternion = (quat: CANNON.Quaternion): THREE.Quaternion =>
   new THREE.Quaternion(quat.x, quat.y, quat.z, quat.w);
 
 const COIN_COLOR = 0xeab308;
+const FLOOR_COLOR = 0x4caf50;
 
 export default (canvas: HTMLCanvasElement) => {
   // Scene
   const scene = new THREE.Scene();
+
+  // Camera
   const camera = new THREE.PerspectiveCamera(
     75,
     canvas.clientWidth / canvas.clientHeight,
@@ -21,9 +25,12 @@ export default (canvas: HTMLCanvasElement) => {
   );
   camera.position.set(0, 3, 4);
   camera.lookAt(0, 0, 0);
+
+  // Renderer
   const renderer = new THREE.WebGLRenderer({ alpha: true, canvas });
   renderer.setSize(canvas.clientWidth, canvas.clientHeight);
   renderer.shadowMap.enabled = true;
+
   // Controls
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.update();
@@ -34,7 +41,11 @@ export default (canvas: HTMLCanvasElement) => {
   });
   world.broadphase = new CANNON.NaiveBroadphase();
   (world.solver as CANNON.GSSolver).iterations = 10;
+
   world.allowSleep = true;
+
+  world.defaultContactMaterial.contactEquationStiffness = 5e6;
+  world.defaultContactMaterial.contactEquationRelaxation = 3;
 
   // Optimizations
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -42,12 +53,24 @@ export default (canvas: HTMLCanvasElement) => {
   camera.updateProjectionMatrix();
 
   // Coin
-  const geometry = new THREE.CylinderGeometry(1, 1, 0.2, 100);
+  const COIN_PARAMS = {
+    radiusTop: 1,
+    radiusBottom: 1,
+    height: 0.2,
+    radialSegments: 100,
+  };
+  const geometry = new THREE.CylinderGeometry(...Object.values(COIN_PARAMS));
+  // TODO: change one side of the coin to a different color
   const coinMaterial = new THREE.MeshPhongMaterial({ color: COIN_COLOR });
   const coinMesh = new THREE.Mesh(geometry, coinMaterial);
+  coinMesh.name = 'coin';
   coinMesh.castShadow = true;
-  const CoinShape = new CANNON.Cylinder(1, 1, 0.2, 100);
   coinMesh.position.set(0, 4, 0);
+  coinMesh.matrixWorldNeedsUpdate = true;
+  scene.add(coinMesh);
+
+  // Add the coin to the physics world
+  const coinShape = new CANNON.Cylinder(...Object.values(COIN_PARAMS));
   const coinBody = new CANNON.Body({
     mass: 1,
     position: new CANNON.Vec3(
@@ -55,46 +78,60 @@ export default (canvas: HTMLCanvasElement) => {
       coinMesh.position.y,
       coinMesh.position.z
     ),
-    shape: CoinShape,
+    shape: coinShape,
     allowSleep: true,
   });
-  coinMesh.name = 'coin';
   world.addBody(coinBody);
-  scene.add(coinMesh);
+
+  coinBody.allowSleep = true;
+  coinBody.sleepSpeedLimit = 0.1; // Body will feel sleepy if speed<1 (speed == norm of velocity)
+  coinBody.sleepTimeLimit = 1; // Body falls asleep after 1s of sleepiness
 
   // Floor
   const floorGeometry = new THREE.PlaneGeometry(10, 10);
-  const floorMaterial = new THREE.MeshPhongMaterial({ color: 0x4caf50 });
+  const floorMaterial = new THREE.MeshPhongMaterial({ color: FLOOR_COLOR });
   const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
   floorMesh.name = 'floor';
-  const floorShape = new CANNON.Plane();
-  const floorBody = new CANNON.Body({
-    shape: floorShape,
-    type: CANNON.Body.STATIC,
-  });
-  floorBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
   floorMesh.receiveShadow = true;
   floorMesh.rotation.x = -Math.PI / 2;
   floorMesh.position.y = -0.0001;
   scene.add(floorMesh);
+
+  // Add the floor to the physics world
+  const floorShape = new CANNON.Plane();
+  const floorBody = new CANNON.Body({
+    shape: floorShape,
+    type: CANNON.Body.STATIC,
+    mass: 0,
+  });
+  floorBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
   world.addBody(floorBody);
 
   // Implement the texture swapping mechanism
   // TODO: Add code to apply materials and textures
 
   // Lights
-  const light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(0, 4, 1);
-  scene.add(light);
+  var dirLight = new THREE.DirectionalLight(0xffffff, 0.8);
+  dirLight.position.set(0, 1, 1);
+  dirLight.castShadow = true;
+  dirLight.shadow.camera.top = 0.2;
+  dirLight.shadow.camera.bottom = -0.2;
+  dirLight.shadow.camera.left = -0.2;
+  dirLight.shadow.camera.right = 0.2;
+  dirLight.shadow.camera.near = 0.1;
+  dirLight.shadow.camera.far = 2;
+  dirLight.shadow.mapSize.set(1024, 1024);
+  scene.add(dirLight);
 
   // light that illuminates the whole scene
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  const ambientLight = new THREE.AmbientLight(0xcba10d, 0.2);
   scene.add(ambientLight);
 
   // light that illuminates the coin
-  const coinLight = new THREE.PointLight(0xf3f4f5, 1);
-  coinLight.position.set(0, 4, 0);
-  scene.add(coinLight);
+  const pointLight = new THREE.PointLight(0xf3f4f5, 1, 0, 2);
+  pointLight.position.set(0, 4, 0);
+  pointLight.castShadow = true;
+  scene.add(pointLight);
 
   // Resize the canvas when the window is resized
   window.addEventListener('resize', () => {
@@ -103,60 +140,98 @@ export default (canvas: HTMLCanvasElement) => {
     camera.updateProjectionMatrix();
   });
 
+  // Coin Animation
+  let isCoinMoving = false;
+
+  coinBody.addEventListener('sleep', () => {
+    isCoinMoving = false;
+
+    console.log('Coin sleeping!', coinBody.velocity.y);
+
+    if (coinBody.velocity.y === 0) zoomToCoin();
+  });
+
   // Make the coin jump when the user clicks on the coin
   const onCoinClick = () => {
-    coinBody.applyLocalImpulse(
-      new CANNON.Vec3(0, 0, -1),
-      new CANNON.Vec3(0, 0, 0)
-    );
+    if (isCoinMoving) return;
 
-    coinLight.intensity = 2;
+    controls.enabled = false;
+
+    console.log('camera', camera.position);
+
+    const impulse = new CANNON.Vec3(500 / 60, 13.5, 0);
+    coinBody.applyImpulse(impulse);
+
+    pointLight.intensity = 2;
+
+    isCoinMoving = true;
 
     console.log('Coin clicked!');
 
-    coinMaterial.color.setHex(0x4caf50);
-
-    setTimeout(() => {
-      coinLight.intensity = 1;
-      coinMaterial.color.setHex(COIN_COLOR);
-    }, 100);
+    coinMaterial.color.setHex(FLOOR_COLOR);
   };
 
-  let mouse = new THREE.Vector2();
-  let boundingBox = new THREE.Box3().setFromObject(coinMesh);
-  const raycaster = new THREE.Raycaster();
+  // Move the camera to the coin when the user clicks on the coin
+  const zoomToCoin = () => {
+    //use this code for a object size agnostic solution; build a bounding box around your object you want to zoom and  get its size:
+    const aabb = new THREE.Box3().setFromObject(coinMesh);
+    const center = aabb.getCenter(new THREE.Vector3());
+
+    gsap.to(camera.position, {
+      duration: 1,
+      x: center.x,
+      y: center.y + 3,
+      z: center.z + 4,
+      ease: 'power1.in',
+
+      onComplete: () => {
+        camera.lookAt(coinMesh.position);
+      },
+    });
+
+    gsap.to(controls.target, {
+      duration: 1,
+      x: center.x,
+      y: center.y, //set the center of the controler to the zoomed object
+      z: center.z, // no distance needed
+      onComplete: () => {
+        controls.enabled = true; // activate the controler again after animation
+      },
+    });
+  };
+
+  // let mouse = new THREE.Vector2();
+  // const raycaster = new THREE.Raycaster();
 
   canvas.addEventListener(
-    'click',
+    'mousedown',
     (event) => {
-      mouse.x = (event.clientX / canvas.clientWidth) * 2 - 1;
-      mouse.y = -(event.clientY / canvas.clientHeight) * 2 + 1;
+      // mouse.x = (event.clientX / canvas.clientWidth) * 2 - 1;
+      // mouse.y = -(event.clientY / canvas.clientHeight) * 2 + 1;
 
       console.log('Canvas clicked!');
+      onCoinClick();
 
-      raycaster.setFromCamera(mouse, camera);
+      // raycaster.setFromCamera(mouse, camera);
 
-      const intersects = raycaster.intersectObject(floorMesh);
+      // const intersects = raycaster.intersectObject(coinMesh);
 
-      const isCoinIntersected = intersects.length > 0;
+      // console.log({ intersects });
 
-      if (isCoinIntersected) {
-        console.log('Coin intersected!', intersects);
-
-        const intersect = intersects[0];
-
-        if (intersect.object === coinMesh) {
-          onCoinClick();
-        }
-      }
-
-      // const isFloorIntersected = intersects.find(
-      //   (intersect) => intersect.object.uuid === floorMesh.uuid
-      // );
-
-      // if (isFloorIntersected) {
-      //   console.error('Floor intersected!', intersects);
+      // if (intersects.length > 0) {
+      //   onCoinClick();
       // }
+    },
+    false
+  );
+
+  canvas.addEventListener(
+    'mouseup',
+    (event) => {
+      setTimeout(() => {
+        pointLight.intensity = 1;
+        coinMaterial.color.setHex(COIN_COLOR);
+      }, 100);
     },
     false
   );
@@ -169,12 +244,8 @@ export default (canvas: HTMLCanvasElement) => {
 
     coinMesh.position.copy(vec3ToThreeVector(coinBody.position));
     coinMesh.quaternion.copy(cannonToThreeQuaternion(coinBody.quaternion));
-    coinMesh.matrixWorldNeedsUpdate = true; // Add this line
-
-    boundingBox.setFromObject(coinMesh);
 
     renderer.render(scene, camera);
-
     controls.update();
   };
   animate();
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,37 @@
 <template>
-  <Jumbo class="bg-yellow-500 text-black min-h-[400px]">
+  <Jumbo class="bg-yellow-500 text-black min-h-[100dvh] relative overflow-hidden">
     <div class="self-center">
-      <PrimitivesFTitle>"Flip The Coin", the online game</PrimitivesFTitle>
-      <p class="mb-3">
-        Have you ever let fate decide for you? You certainly shouldn't!
-      </p>
-      <UButton color="white" to="/play" size="md">Play!</UButton>
+      <canvas
+        ref="canvasRef"
+        class="w-full h-[80dvh] mt-4 absolute top-0 right-0"
+        :class="{
+          'cursor-pointer': !isFlipping,
+          'cursor-grab': isIntersecting && !isFlipping,
+        }"
+      ></canvas>
+      <div class="isolate z-10">
+        <PrimitivesFTitle>"Flip The Coin", the online game</PrimitivesFTitle>
+        <p class="mb-3">
+          Have you ever let fate decide for you? You certainly shouldn't!
+        </p>
+        <UButton
+          color="white"
+          @click="flipCoin"
+          :disabled="isFlipping"
+          size="md"
+        >
+          {{ isFlipping ? 'Flipping...' : 'Flip the coin' }}
+        </UButton>
+        <p
+          class="mt-3"
+          v-if="result"
+        >{{ result }}</p>
+      </div>
+
     </div>
   </Jumbo>
   <Jumbo class="min-h-[400px]">
-    <PrimitivesFTitle as="h2"
-      >The game to flip coins and complete challenges.</PrimitivesFTitle
-    >
+    <PrimitivesFTitle as="h2">The game to flip coins and complete challenges.</PrimitivesFTitle>
     <p>
       The game of uncertainty: Tossing a coin gives us a 50% chance of it
       landing heads and 50% chance landing tails, but until the coin lands we
@@ -35,10 +55,8 @@
       />
     </svg>
     <blockquote>
-      <em
-        >"You are what you are today because of the choices you made yesterday,
-        and the choices you make today will make you what you are tomorrow."</em
-      >
+      <em>"You are what you are today because of the choices you made yesterday,
+        and the choices you make today will make you what you are tomorrow."</em>
       — Michael Josephson
     </blockquote>
   </Jumbo>
@@ -57,17 +75,299 @@
       The game ends when you complete all the challenges or when you give up.
     </p>
   </Jumbo> -->
-  <Jumbo class="min-h-[400px] flex">
-    <canvas ref="canvas" class="w-full grow"></canvas>
-  </Jumbo>
 </template>
 
 <script setup lang="ts">
-import createCoin from '../coin';
+import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { gsap } from 'gsap';
 
-const canvas = ref<HTMLCanvasElement | null>(null);
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+const isFlipping = ref(false);
+const result = ref('');
+const tossCount = ref(0);
+const previousTossCount = ref(0);
+const isIntersecting = ref(false);
+
+let scene: THREE.Scene,
+  camera: THREE.PerspectiveCamera,
+  renderer: THREE.WebGLRenderer,
+  world: CANNON.World,
+  coinMesh: THREE.Mesh,
+  coinBody: CANNON.Body,
+  floorMesh: THREE.Mesh,
+  floorBody: CANNON.Body,
+  raycaster: THREE.Raycaster,
+  mouse: THREE.Vector2,
+  controls: OrbitControls,
+  mainLight: THREE.DirectionalLight;
+
+const DEFAULT_CAMERA_POSITION = new THREE.Vector3(-5, 4, 1);
+
+const initThreeJS = () => {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer({ canvas: canvasRef.value!, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // make the shadows look softer
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  // make the canvas transparent
+  renderer.setClearColor(0x000000, 0);
+  // controls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.25;
+  controls.update()
+  // initialises raycaster and mouse vector
+  raycaster = new THREE.Raycaster();
+  mouse = new THREE.Vector2();
+
+  // Ambient light
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1.1);
+  scene.add(ambientLight);
+
+  // Main directional light (sun-like)
+  mainLight = new THREE.DirectionalLight(0xffffff, 1.1);
+  mainLight.position.set(5, 10, 7);
+  mainLight.castShadow = true;
+  mainLight.shadow.mapSize.width = 1024;
+  mainLight.shadow.mapSize.height = 1024;
+  scene.add(mainLight);
+
+  // Fill light
+  const fillLight = new THREE.DirectionalLight(0xffffff, 2);
+  fillLight.position.set(-5, 3, -5);
+  scene.add(fillLight);
+
+  // Adjust camera position
+  camera.position.copy(DEFAULT_CAMERA_POSITION);
+  camera.lookAt(0, 0, 0);
+};
+
+const createCoin = () => {
+  const radius = 1;
+  const height = 0.1;
+  const segments = 32;
+
+  const geometry = new THREE.CylinderGeometry(radius, radius, height, segments);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xD4AF37,  // Golden color
+    metalness: 0.7,
+    roughness: 0.3,
+  });
+
+  coinMesh = new THREE.Mesh(geometry, material);
+  coinMesh.castShadow = true;
+
+  // Create textures for heads and tails
+  const textureLoader = new THREE.TextureLoader();
+  const headsTexture = textureLoader.load('/img/head.webp');
+  const tailsTexture = textureLoader.load('/img/tails.webp');
+
+  // Create materials for the faces
+  const headsMaterial = new THREE.MeshStandardMaterial({ map: headsTexture });
+  const tailsMaterial = new THREE.MeshStandardMaterial({ map: tailsTexture });
+
+  // Apply materials to the top and bottom faces
+  coinMesh.material = [
+    material,            // Side
+    headsMaterial,       // Top
+    tailsMaterial        // Bottom
+  ];
+
+  coinMesh.position.set(0, 1, 0);
+  scene.add(coinMesh);
+
+  // Physical representation
+  const coinShape = new CANNON.Cylinder(radius, radius, height, segments);
+  coinBody = new CANNON.Body({
+    mass: 1,
+    shape: coinShape,
+  });
+  coinBody.position.copy(coinMesh.position as any);
+  world.addBody(coinBody);
+};
+
+const createFloor = () => {
+  // Visual representation to show shadows
+  const floorGeometry = new THREE.PlaneGeometry(100, 100);
+  // set to ShadowMaterial to receive shadows but not be visible
+  const floorMaterial = new THREE.ShadowMaterial({ opacity: 0.5 });
+  floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
+  floorMesh.rotation.x = -Math.PI / 2;
+  floorMesh.receiveShadow = true;
+  scene.add(floorMesh);
+
+  // Physical representation
+  floorBody = new CANNON.Body({
+    type: CANNON.Body.STATIC,
+    shape: new CANNON.Plane(),
+  });
+  floorBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+  world.addBody(floorBody);
+};
+
+const initPhysics = () => {
+  world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.82, 0),
+  });
+};
+
+const flipCoin = () => {
+  if (isFlipping.value) return;
+  controls.enabled = false;
+  result.value = '';
+  isFlipping.value = true;
+  tossCount.value++;
+
+  const flipForce = 9;
+  const randomX = Math.random() * 0.5 - 0.25;
+  const randomZ = Math.random() * 0.5 - 0.25;
+
+  coinBody.position.set(coinMesh.position.x, coinMesh.position.y, coinMesh.position.z);
+  coinBody.velocity.set(randomX, flipForce, randomZ);
+  coinBody.angularVelocity.set(Math.random() * 20 - 10, Math.random() * 20 - 10, 0);
+};
+
+const determineResult = () => {
+  // Create a vector pointing up (0, 1, 0) and transform it by the coin's rotation
+  const up = new THREE.Vector3(0, 1, 0);
+  up.applyQuaternion(coinMesh.quaternion);
+
+  // If the dot product of the up vector and the world's up vector is positive, the coin is heads
+  const isHeads = up.dot(new THREE.Vector3(0, 1, 0)) > 0;
+
+  result.value = isHeads ? 'Heads' : 'Tails';
+
+  // if the rotation is vertical, the coin is standing on its edge
+  if (Math.abs(up.y) < 0.1) {
+    result.value = 'Edge';
+  }
+
+
+  console.log('Result:', result.value);
+};
+
+// Event listeners
+
+function onPointerMove(event: PointerEvent) {
+  // calculate pointer position in normalized device coordinates
+  // (-1 to +1) for both components
+  mouse.x = (event.clientX / canvasRef.value!.clientWidth) * 2 - 1;
+  mouse.y = - (event.clientY / canvasRef.value!.clientHeight) * 2 + 1;
+
+}
+
+function onCanvasClick() {
+  // if (isIntersecting.value) {
+  flipCoin();
+  // }
+}
+
+const initListeners = () => {
+  canvasRef.value!.addEventListener('pointermove', onPointerMove);
+  canvasRef.value!.addEventListener('click', onCanvasClick);
+};
+
+const destroyListeners = () => {
+  canvasRef.value!.removeEventListener('pointermove', onPointerMove);
+  canvasRef.value!.removeEventListener('click', onCanvasClick);
+};
+const zoomToCoin = () => {
+  //use this code for a object size agnostic solution; build a bounding box around your object you want to zoom and  get its size:
+  const aabb = new THREE.Box3().setFromObject(coinMesh);
+  const center = aabb.getCenter(new THREE.Vector3());
+
+  gsap.to(camera.position, {
+    duration: .3,
+    x: center.x + DEFAULT_CAMERA_POSITION.x,
+    y: DEFAULT_CAMERA_POSITION.y,
+    z: center.z + DEFAULT_CAMERA_POSITION.z,
+    ease: 'power1.inOut',
+  });
+
+  gsap.to(controls.target, {
+    duration: .2,
+    x: center.x,
+    y: center.y, //set the center of the controler to the zoomed object
+    z: center.z, // no distance needed
+    onComplete: () => {
+      controls.enabled = true; // activate the controler again after animation
+    },
+  });
+};
+
+const updateLightPosition = (pos: THREE.Vector3) => {
+  gsap.to(mainLight.position, {
+    x: pos.x + 5,
+    y: pos.y + 10,
+    z: pos.z + 7,
+    duration: 1,
+  });
+};
+
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  world.step(1 / 60);
+
+  raycaster.setFromCamera(mouse, camera);
+
+
+  // Update the coin's position and rotation
+  if (coinMesh && coinBody) {
+    coinMesh.position.copy(coinBody.position as any);
+    coinMesh.quaternion.copy(coinBody.quaternion as any);
+
+    // Check if the mouse is intersecting with the coin
+    isIntersecting.value = raycaster.intersectObject(coinMesh).length > 0;
+  }
+
+
+  if (isFlipping.value) {
+    // Move the floor to follow the coin
+    floorMesh.position.set(coinBody.position.x, 0, coinBody.position.z);
+
+  }
+
+  // When the coin is decreasing its speed but it's still moving
+  if (isFlipping.value && coinBody && coinBody.velocity.y < 0.1) {
+    zoomToCoin();
+  }
+
+  // Check if the coin has stopped moving
+  if (
+    coinBody
+    && Math.abs(coinBody.angularVelocity.y) < 0.01
+    && Math.abs(coinBody.angularVelocity.x) < 0.01
+  ) {
+    isFlipping.value = false;
+
+    if (tossCount.value > previousTossCount.value) {
+      zoomToCoin();
+      updateLightPosition(coinMesh.position);
+      determineResult();
+      previousTossCount.value = tossCount.value;
+    }
+  }
+
+
+  renderer.render(scene, camera);
+};
 
 onMounted(() => {
-  createCoin(canvas.value!);
+  initThreeJS();
+  initPhysics();
+  createFloor();
+  createCoin();
+  animate();
+  initListeners();
+});
+
+onUnmounted(() => {
+  destroyListeners();
 });
 </script>
