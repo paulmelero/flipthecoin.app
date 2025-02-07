42ba2d56aeedb835b80c148dc9946a57b7847202
Paul <paul.melero@backmarket.com>
2025-02-07 07:35:18 +0100

    made monorepo

---
+++ packages/shared/src/constants.ts
@@ -0,0 +1,3 @@
+export const TITLE = 'FlipTheCoin.app - Do you need a coin?';
+export const DESCRIPTION =
+  "The place where coins fairly flip. What is random? What is your fait? Please don't leave a coin decide it.";
