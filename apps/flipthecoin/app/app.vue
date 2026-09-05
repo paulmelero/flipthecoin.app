7686bc01299b80f679b5cf75dae33875c20778f2
paulmelero <paul.melero@backmarket.com>
2026-09-06 01:03:05 +0200

    optimise whatsapp img preview

--- apps/flipthecoin/app/app.vue
+++ apps/flipthecoin/app/app.vue
@@ -55,7 +55,13 @@ useHead({
     { property: 'og:description', content: description.value as string },
     { property: 'og:type', content: 'website' },
     { property: 'og:image', content: ogImage },
+    { property: 'og:image:width', content: 1200 },
+    { property: 'og:image:height', content: 630 },
     { property: 'og:url', content: canonicalUrl },
+    {
+      property: 'og:locale',
+      content: $getLocale() === 'es' ? 'es_ES' : 'en_US',
+    },
     { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:title', content: TITLE },
     { name: 'twitter:description', content: description.value as string },
